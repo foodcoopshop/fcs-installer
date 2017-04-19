dc042ad8323f0c2054433eb77e6e7c51d2bf0718
Michael Kramer <info@k-pd.de>
2017-04-19 11:59:39 +0200

    Use latest coding standards from main project

--- devtools/setup-git.sh
+++ devtools/setup-git.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
 
-################################################################################
-#
-#  The script below is derived from http://tech.zumba.com/2014/04/14/control-code-quality/
-#
-################################################################################
-
-cp devtools/git-pre-commit.sh .git/hooks/pre-commit
+cp -f devtools/pre-commit .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
-cp devtools/git-pre-push.sh .git/hooks/pre-push
-chmod +x .git/hooks/pre-push
